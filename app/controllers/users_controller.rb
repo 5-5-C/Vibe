c08883c591b67614bd2451636cb4bce3aa70385d
Graham Moss <graham.moss@hotmail.com>
2017-02-28 16:12:44 -0500

    fully working collapsing navbar, auto-login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path
+      session[:user_id] = @user.id
+
+      redirect_to root_url
     else
       render "new"
     end
@@ -21,6 +23,7 @@ class UsersController < ApplicationController
   end
 
   private
+
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
