374b51f093a08c5d7998019729f1fe11d2448f00
Amena <ame1.khatun@gmail.com>
2017-03-02 14:59:09 -0500

    implemented dob and organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   has_many :time_slots
   has_many :events, through: :time_slots
   has_secure_password
-
-
   REGIONS = ["City of Toronto", "Peel", "Durham", "Halton", "York"]
+
+  def age
+    ((Time.now - self.date_of_birth.to_time)/ 1.year ).round
+  end
+
 end
+
 
 # def
 # u = User.new
