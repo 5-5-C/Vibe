8f508484210d3a7b692ee713f8e6a8ab85cf7dfd
Amena <ame1.khatun@gmail.com>
2017-03-04 13:09:21 -0500

    took out some doubles in the user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,4 @@ class User < ApplicationRecord
   # Explicitly do not validate
   do_not_validate_attachment_file_type :picture
 
-
-
-
-  has_attached_file :picture, styles: {thumbnail: ["10x10#"]}
- # validates_attachment :picture,  content_type: ["image/jpeg", "image/gif", "image/png"]
-   has_attached_file :picture
-   # Validate content type
-   validates_attachment_content_type :picture, content_type: /\Aimage/
-   # Validate filename
-   validates_attachment_file_name :picture, matches: [/png\z/, /jpe?g\z/]
-   # Explicitly do not validate
-   do_not_validate_attachment_file_type :picture
-
 end
