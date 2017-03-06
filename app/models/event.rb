624269650c06bd3b0ee1da444d7beb4d9b639ee2
Graham Moss <graham.moss@hotmail.com>
2017-03-06 14:22:48 -0500

    Add error message toevent form, add date cant be in the past validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :organization
-  has_many :timeslots
+
+  validate :date_cannot_be_in_the_past
+
+def date_cannot_be_in_the_past
+  if date.present? && date < Date.today
+    errors.add(:date, "can't be in the past")
+  end
+end
 
 end
