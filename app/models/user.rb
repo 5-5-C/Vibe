146bdb3b8eea58d94c50a24737df260b92a45f64
5-5-C <jmarshall.brock@gmail.com>
2017-03-07 16:02:25 -0500

    final merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,12 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
+
   has_many :events
-=======
-  has_many :timeslots
-  has_many :events, through: :timeslots
-  validates_presence_of :hours
->>>>>>> 9357dfdd9a7b32c970a8d27fd8e31937ac255a28
   has_secure_password
 
 # validates :first_name, :last_name, :email, :password_digest, presence: true
 # validates :first_name, :last_name, :email, uniqueness: true
 
   REGIONS = ["City of Toronto", "Peel", "Durham", "Halton", "York"]
-
-  def age
-    ((Time.now - self.date_of_birth.to_time)/ 1.year ).round
-  end
 
   def age
     ((Time.now - self.date_of_birth.to_time)/ 1.year).round
