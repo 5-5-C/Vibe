2698267acc5e6ced8892aed50b45c2a1513b2807
5-5-C <jmarshall.brock@gmail.com>
2017-03-10 12:26:47 -0500

    added styling

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,12 +1,12 @@
-$(window).scroll(function(){
-
-  var wScroll = $(this).scrollTop();
-if(wScroll > $('#about-us').offset().top - ($(window).height() / 1.4) && ($("#about-us").attr("class") != "is-visible")) {
-
-$('#about-us').attr("class","is-visible about-us");
-
-
-}
-
-
-});
+// $(window).scroll(function(){
+//
+//   var wScroll = $(this).scrollTop();
+// if(wScroll > $('#about-us').offset().top - ($(window).height() / 1.4) && ($("#about-us").attr("class") != "is-visible")) {
+//
+// $('#about-us').attr("class","is-visible about-us");
+//
+//
+// }
+//
+//
+// });
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,70 +10,75 @@ Organization.create(
 name: "St. John's church",
 location: "222 Bathurst st.",
 description: "Church",
-imageURL:
+image_url: "http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Women's Shelter Toronto",
 location: "777 Yonge St.",
 description: "Womens Shelter",
-imageURL:
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Toronto Homeless Shelter",
 location: "111 King St.",
 description: "Shelter",
-imageURL:
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Second Chance Rescue",
 location: "333 Queen St.",
 description: "Dog Shelter",
-imageURL:
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Make A Wish",
 location: "444 Front St.",
 description: "Children's Charity",
-imageURL:
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Plant a Tree ",
 location: "961 Planters lane",
 description: "Tree Planting",
-imageURL:
-
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
+)
 
 Organization.create(
 name: "Angel Emergency Alert Organization",
 location: "111 Richmond St W #12, Toronto, ON M5H 2G4",
 description: "Google Cardboard Virtual Reality Volunteers",
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Belmont House",
 location: " 55 Belmont St, Toronto, ON M5R 1R1",
 description: "Recreation Helper for Senior Activities/Leisure Game",
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Luminato",
 location: "180 Shaw Street, Suite 301 Toronto, ON, M6J 2W5",
 description: "Youth Photography Program Participants",
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "High Park Nature Centre",
 location: "375 Colborne Lodge Dr, Toronto, ON M6R 2Z3",
 description: "OURSpace Assistant",
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
 
 Organization.create(
 name: "Miles Nadal JCC",
 location: "750 Spadina Ave, Toronto, ON M5S 2J2",
 description: "Floor Hockey Academy Assistant",
+image_url:"http://glendalecitychurch.org/wp-content/uploads/2015/01/temp-citychurch-outside.jpg",
 )
