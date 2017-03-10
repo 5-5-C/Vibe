c5f048f80754ce268540cd5a8613ddb7ced92acb
Graham Moss <graham.moss@hotmail.com>
2017-03-09 22:13:34 -0500

    fix user profile problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, :password_digest, presence: true
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
 
 
   REGIONS = ["City of Toronto", "Peel", "Durham", "Halton", "York"]
