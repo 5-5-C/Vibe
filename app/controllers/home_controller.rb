3ce6930c389550fc0665cabeeaa93184e2a24bc8
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-03-16 16:39:34 -0400

    add events happening soon to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @events = Event.all
     @events = Event.order('created_at DESC').first(3)
+    @events = Event.order(:date)
+
   end
 end
