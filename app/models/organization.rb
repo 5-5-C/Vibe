5f15096692de1895f21f204b3d3488ea972dfa9a
Graham Moss <graham.moss@hotmail.com>
2017-03-16 18:54:01 -0400

    Add search to org index, add event show page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,12 @@
 class Organization < ApplicationRecord
   has_many :events
   belongs_to :user
-end
 
   def create_event
     render form_for @event
   end
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
+end
