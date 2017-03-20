b0b530d678cbae7b716bfd84c8a170aa12b49830
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-03-20 17:11:42 -0400

    add summary to events seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310085614) do
+ActiveRecord::Schema.define(version: 20170320205933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170310085614) do
     t.time     "start_time"
     t.time     "end_time"
     t.integer  "capacity"
+    t.string   "summary"
   end
 
   create_table "events_users", force: :cascade do |t|
