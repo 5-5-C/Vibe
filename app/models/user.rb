86643d2b6ac19951532ecde13be70f5e9a7c4dd6
Amena <ame1.khatun@gmail.com>
2017-03-21 00:02:54 -0400

    added validations to users update page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   validates :email, :password_digest, presence: true
   # validates :email, uniqueness: true
 
+  validates_presence_of :first_name, :last_name, :date_of_birth, :goals, :skills,  on: :update
+
 
   SKILLS = ["","Administrative", "Analytical", "Artistic/Creative", "Budgeting",
   "Communicaton", "Computer", "Conflict Resolution", "Creating Ideas",
@@ -21,30 +23,29 @@ class User < ApplicationRecord
 
   REGIONS = ["", "Annex", "Beaches", "Bloor West", "Danforth", "Downtown", "Etobicoke", "Midtown", "North York", "Scarborough"]
 
-
   def age
     ((Time.now - self.date_of_birth.to_time)/ 1.year ).round
   end
 
 
-  # has_attached_file :picture, styles: {thumbnail: ["10x10#"]}
- # validates_attachment :picture,  content_type: ["image/jpeg", "image/gif", "image/png"]
-  has_attached_file :picture
-  # Validate content type
-  validates_attachment_content_type :picture, content_type: /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :picture, matches: [/png\z/, /jpe?g\z/]
-  # Explicitly do not validate
-  do_not_validate_attachment_file_type :picture
+    # has_attached_file :picture, styles: {thumbnail: ["10x10#"]}
+    # validates_attachment :picture,  content_type: ["image/jpeg", "image/gif", "image/png"]
+    has_attached_file :picture
+    # Validate content type
+    validates_attachment_content_type :picture, content_type: /\Aimage/
+    # Validate filename
+    validates_attachment_file_name :picture, matches: [/png\z/, /jpe?g\z/]
+    # Explicitly do not validate
+    do_not_validate_attachment_file_type :picture
 
-end
+  end
 
-def all_skills
-  skill = Skill.select(params)
-  current_user.skills << skills
-end
+  def all_skills
+    skill = Skill.select(params)
+    current_user.skills << skills
+  end
 
 
-def a_method_used_for_validation_purposes
-    errors.add(:name, "You need a name!")
-end
+  def a_method_used_for_validation_purposes
+      errors.add(:name, "You need a name!")
+  end
